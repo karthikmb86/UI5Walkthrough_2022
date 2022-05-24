4446226056ceece3d934bd17c9192c0a1c17d50a
KarthikBangera <karthikmb86@gmail.com>
2022-05-24 14:30:25 +1000

    walkthough

---
+++ proxy.js
@@ -0,0 +1,12 @@
+const corsProxy = require("cors-anywhere");
+
+const host = "0.0.0.0";
+const PORT = 8081;
+
+corsProxy.createServer({
+    originWhitelist: [],  //Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"]
+}).listen(PORT,host, () => {
+    console.log(`Running CORS Anywhere on ${host} " : " ${PORT}`);
+})
